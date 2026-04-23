fee8c77bf3497c08b91a6a986554144d7d7fe21a
wIvy <fsaidad54673@yandex.ru>
2026-04-23 18:53:03 +0300

    prepare-assets_1

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/router";
 import "./styles/index.scss";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
