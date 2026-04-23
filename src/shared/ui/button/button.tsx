ff9fc759d8d12f68c2c279b80c49b6fc3d546a46
Gulnaz <gulnaz.davletshina.97@mail.ru>
2026-04-23 22:33:16 +0300

    feat: добавлена настройка boundaries/no-unknown-files и расставлено по FSD

---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,30 @@
+import type { ComponentProps } from "react"
+import { type VariantProps } from "class-variance-authority"
+import { Slot } from "radix-ui"
+import { buttonVariants } from "./button-variants"
+import { cn } from "@/shared/lib"
+
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  asChild = false,
+  ...props
+}: ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot.Root : "button"
+
+  return (
+    <Comp
+      data-slot="button"
+      data-variant={variant}
+      data-size={size}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
+
+export { Button }
