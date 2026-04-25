29ad03bfd2ed62e3dffbaefd39fe56016d1ea44c
Nik-s-har <nick762@yandex.ru>
2026-04-25 12:38:27 +0300

    feat: add Calendar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,17 @@
 import { Outlet } from "react-router";
+import { useState } from "react";
+import { Calendar } from "@ui/calendar";
 
 export function App() {
+   const [date, setDate] = useState<Date | undefined>(new Date())
   return (
     <div className="app-layout">
+      <Calendar 
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-lg border"
+      />
       {/* тут размещается sidebar */}
       <Outlet />
     </div>
