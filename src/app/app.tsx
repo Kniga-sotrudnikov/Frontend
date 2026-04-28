0a29d5d987160a473b323276fbeec64420616cd6
Nik-s-har <nick762@yandex.ru>
2026-04-28 17:27:14 +0300

    Remove Calendar from App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,8 @@
 import { Outlet } from "react-router";
-import { useState } from "react";
-import { Calendar } from "@/shared/ui/calendar/calendar";
 
 export function App() {
-   const [date, setDate] = useState<Date | undefined>(new Date())
   return (
     <div className="app-layout">
-      <Calendar 
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-lg border"
-      />
       {/* тут размещается sidebar */}
       <Outlet />
     </div>
