cc67b6413ea993c465f72a261b3f0a8d6819ff8e
Viktor Babenko <baumschule103@gmail.com>
2026-04-28 19:56:25 +0300

    удаляет комментарии

--- src/shared/model/stores/use-notification-store.ts
+++ src/shared/model/stores/use-notification-store.ts
@@ -1,28 +1,21 @@
 import { create } from "zustand";
 
-// Типы уведомлений. Возможно надо будет изменить.
-// Предпологается, что от них будет зависеть иконка и цвет(?) уведомления
 export type NotificationType = "success" | "error" | "info" | "warning";
-
-// Интерфейс одного уведомления
 export interface Notification {
   id: string;
   type: NotificationType;
-  title: string; //У марии день рождения | Карточка сотрудника создана
-  message?: string; //Не забудьте поздравить
-  actions?: Array<{ label: string; onClick: () => void }>; //Кнопки опционально
+  title: string;
+  message?: string;
+  actions?: Array<{ label: string; onClick: () => void }>;
   duration?: number; // Время до авто-закрытия (мс), 0 = не закрывать
 }
-
-// Интерфейс стора
 interface NotificationStore {
   notifications: Notification[];
-  add: (notification: Omit<Notification, "id">) => void; // Тип Notification но без id, т.к. он генерируется внутри add
+  add: (notification: Omit<Notification, "id">) => void;
   remove: (id: string) => void;
   clearAll: () => void;
 }
 
-// Создаём стор
 export const useNotificationStore = create<NotificationStore>((set) => ({
   notifications: [],
 
@@ -30,12 +23,10 @@ export const useNotificationStore = create<NotificationStore>((set) => ({
     const id = crypto.randomUUID();
     const newNotification = { ...notification, id };
 
-    // Добавляем уведомление
     set((state) => ({
       notifications: [...state.notifications, newNotification],
     }));
 
-    // Авто-удаление (если duration не 0)
     if (notification.duration !== 0) {
       setTimeout(() => {
         set((state) => ({
