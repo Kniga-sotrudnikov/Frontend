a7a89edb33bbe9764f561c68b680cca26c6a36da
Alex Balyanov <alex44298@yandex.ru>
2026-05-01 16:53:46 +0500

    fix: разрешение конфликтов с дев

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -2,18 +2,17 @@ import { Slot } from "@radix-ui/react-slot";
 import { cn } from "@/shared/lib";
 import { buttonVariants } from "./button-variants";
 import { type ButtonProps } from "./button-types";
+import { forwardRef } from "react";
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: ButtonProps) {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  { className, variant, size, asChild = false, ...props },
+  ref,
+) {
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
+      ref={ref}
       data-slot="button"
       data-variant={variant}
       data-size={size}
@@ -21,6 +20,6 @@ function Button({
       {...props}
     />
   );
-}
+});
 
 export { Button };
