b14d051e6dd30de945e0a3958adf7d79be30cf62
Alex Balyanov <alex44298@yandex.ru>
2026-05-04 17:40:10 +0500

    wip: добавил сортировку в таблицу

--- src/shared/ui/table/columns.tsx
+++ src/shared/ui/table/columns.tsx
@@ -1,6 +1,10 @@
 import type { ColumnDef } from "@tanstack/react-table";
 import type { TEmployee } from "@ui/table/types.ts";
 
+import SortDescIcon from "@/shared/assets/icons/sort-1.svg?react";
+import SortAscIcon from "@/shared/assets/icons/sort-2.svg?react";
+import { Button } from "@ui/button";
+
 export const columns: ColumnDef<TEmployee>[] = [
   {
     accessorKey: "name",
@@ -9,27 +13,107 @@ export const columns: ColumnDef<TEmployee>[] = [
   },
   {
     accessorKey: "position",
-    header: "Должность",
     size: 144,
+    header: ({ column }) => {
+      const sortDirection = column.getIsSorted();
+      const SortIcon = sortDirection === "desc" ? SortDescIcon : SortAscIcon;
+
+      return (
+        <div className="flex items-center justify-between">
+          <span>Должность</span>
+          <Button
+            variant="ghost"
+            size="icon-xl"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            <SortIcon className="size-5" />
+          </Button>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "section",
-    header: "Раздел",
     size: 144,
+    header: ({ column }) => {
+      const sortDirection = column.getIsSorted();
+      const SortIcon = sortDirection === "desc" ? SortDescIcon : SortAscIcon;
+
+      return (
+        <div className="flex items-center justify-between">
+          <span>Раздел</span>
+          <Button
+            variant="ghost"
+            size="icon-xl"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            <SortIcon className="size-5" />
+          </Button>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "department",
-    header: "Отдел",
     size: 144,
+    header: ({ column }) => {
+      const sortDirection = column.getIsSorted();
+      const SortIcon = sortDirection === "desc" ? SortDescIcon : SortAscIcon;
+
+      return (
+        <div className="flex items-center justify-between">
+          <span>Отдел</span>
+          <Button
+            variant="ghost"
+            size="icon-xl"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            <SortIcon className="size-5" />
+          </Button>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "status",
-    header: "Статус",
     size: 116,
+    header: ({ column }) => {
+      const sortDirection = column.getIsSorted();
+      const SortIcon = sortDirection === "desc" ? SortDescIcon : SortAscIcon;
+
+      return (
+        <div className="flex items-center justify-between">
+          <span>Статус</span>
+          <Button
+            variant="ghost"
+            size="icon-xl"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            <SortIcon className="size-5" />
+          </Button>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "city",
-    header: "Город",
     size: 132,
+    header: ({ column }) => {
+      const sortDirection = column.getIsSorted();
+      const SortIcon = sortDirection === "desc" ? SortDescIcon : SortAscIcon;
+
+      return (
+        <div className="flex items-center justify-between">
+          <span>Город</span>
+          <Button
+            variant="ghost"
+            size="icon-xl"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            <SortIcon className="size-5" />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
--- src/shared/ui/table/data-table.tsx
+++ src/shared/ui/table/data-table.tsx
@@ -1,10 +1,11 @@
 import {
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
-import type { ColumnDef } from "@tanstack/react-table";
+import type { ColumnDef, SortingState } from "@tanstack/react-table";
 
 import {
   Table,
@@ -14,6 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from "./table.tsx";
+import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -24,10 +26,17 @@ export const DataTable = <TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
