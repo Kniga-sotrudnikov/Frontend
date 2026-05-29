f4a36da2daef84e57e13780693612d8b05cbd1ff
Nik-s-har <126312610+Nik-s-har@users.noreply.github.com>
2026-05-29 18:03:22 +0300

    Feature/employee book button list (#95)
    * Output in a table
    
    * Delete files

--- src/pages/employees/employees-page.tsx
+++ src/pages/employees/employees-page.tsx
@@ -4,16 +4,10 @@ import { HeaderUserCard } from "@/widgets/header-user-card";
 import { BirthdaysPopover } from "@/widgets/birthdays-popover";
 import { Navbar, orgTree } from "@/widgets/navbar";
 import { EmployeesList } from "@/widgets/employees-list";
-import { StatusFilter } from "@/features/employee/ui/status-filter";
-import { CreateEmployeeWrapper } from "@/features/create-employee";
-import { useState } from "react";
-import type { TEmployeeStatus } from "@/entities/employee";
+import { EmployeesFilterBar } from "@/widgets/employees-filter-bar";
 import { mockEmployees, mockFavorites, mockVacancies } from "./mocks/mocks";
-import FilterCities from "@/widgets/filter-cities/filter-cities";
 
 const EmployeesPage = () => {
-  const [value, setValue] = useState<TEmployeeStatus[]>([]);
-
   return (
     <div className="bg-gray-50 min-h-screen">
       <PageHeader
@@ -27,14 +21,8 @@ const EmployeesPage = () => {
       <div className="mx-10 mt-5 grid grid-cols-[295px_1fr] gap-x-7 min-h-screen">
         <Navbar unitsList={orgTree} />
 
-        <div className="space-y-3">
-          <div className="flex gap-x-3">
-            <StatusFilter value={value} onValueChange={setValue} />
-            <FilterCities cities={["Москва", "Сочи"]} />
-            <FilterCities cities={["Москва", "Сочи"]} />
-            <CreateEmployeeWrapper className="ml-auto" />
-          </div>
-
+        <div className="space-y-3 max-w-235">
+          <EmployeesFilterBar />
           <EmployeesList
             employees={mockEmployees}
             vacancies={mockVacancies}
