b88914b83af6b714976709f49061e89c296de285
Alex Balyanov <alex44298@yandex.ru>
2026-06-05 18:12:03 +0500

    fix: фикс конфликтов

--- src/entities/employee/index.ts
+++ src/entities/employee/index.ts
@@ -1,29 +1,16 @@
+export type {
+  TEmployee,
+  EmployeeStatus,
+  TEmployeeStatus,
+  EmployeeData,
+} from "./model/types";
 export type { BirthdayPerson } from "./model/mock";
-export type {
-  TEmployee,
-  EmployeeStatus,
-  TEmployeeStatus,
-  EmployeeData,
-} from "./model/types";
-export type {
-  TEmployee,
-  EmployeeStatus,
-  TEmployeeStatus,
-  EmployeeData,
-  TShortEmployee,
-} from "./model/types";
 export { employees } from "./model/mock";
 export { employeeTableColumns } from "./ui/employee-table-columns";
 export { mapTEmployeeToEmployeeData } from "./utils/map-temployee-to-employee-data";
 export {
-  employees,
-  shortEmployees,
   MOCK_BIRTHDAYS,
   getCurrentMonthBirthdays,
   getTodayBirthdays,
   getUpcomingBirthdays,
 } from "./model/mock";
-export { statusFilterOptions } from "./model/constants";
-export { employeeTableColumns } from "./ui/employee-table-columns";
-export { SelectedEmployee } from "./ui/selected-employee";
-export { mapTEmployeeToEmployeeData } from "./utils/map-temployee-to-employee-data";
