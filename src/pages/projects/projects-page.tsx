b285f870a825bba3ff325bf32a63fbb392a090bd
Alex Balyanov <alex44298@yandex.ru>
2026-06-05 11:01:18 +0500

    chore: убрал SelectedEmployeesList со страницы проектов

--- src/pages/projects/projects-page.tsx
+++ src/pages/projects/projects-page.tsx
@@ -2,20 +2,8 @@ import { SearchInput } from "@/shared/ui/input";
 import { PageHeader } from "@/widgets/page-header";
 import { HeaderUserCard } from "@/widgets/header-user-card";
 import { BirthdaysPopover } from "@/widgets/birthdays-popover";
-import { SelectedEmployeesList } from "@/widgets/selected-employees-list/selected-employees-list.tsx";
-import { shortEmployees, type TShortEmployee } from "@/entities/employee";
-import { useState } from "react";
 
 const ProjectsPage = () => {
-  const [selectedEmployees, setSelectedEmployees] =
-    useState<TShortEmployee[]>(shortEmployees);
-
-  const handleDeleteEmployee = (employeeId: number) => {
-    setSelectedEmployees((prev) =>
-      prev.filter((employee) => employee.id !== employeeId),
-    );
-  };
-
   return (
     <div>
       <PageHeader
@@ -26,12 +14,7 @@ const ProjectsPage = () => {
           <HeaderUserCard name="Алексеева Виктория" position="HR-специалист" />
         }
       />
-      <div className="p-2 w-full max-w-[700px] bg-white">
-        <SelectedEmployeesList
-          employees={selectedEmployees}
-          onDelete={handleDeleteEmployee}
-        />
-      </div>
+      <div className="p-6">Проекты. В работе...</div>
     </div>
   );
 };
