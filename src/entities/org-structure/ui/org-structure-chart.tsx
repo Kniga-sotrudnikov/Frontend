44a0d0b5b6a2b1bd78d4f0e1902e578afa7eccc5
Nik-s-har <nick762@yandex.ru>
2026-06-29 19:28:36 +0300

    Add Notification

--- src/entities/org-structure/ui/org-structure-chart.tsx
+++ src/entities/org-structure/ui/org-structure-chart.tsx
@@ -1,3 +1,4 @@
+import { useNotificationStore } from "@/shared/model/stores";
 import { Skeleton } from "@/shared/ui/skeleton";
 import { ZoomableImage } from "@/shared/ui/zoomable-image";
 import { ZoomablePDF } from "@/shared/ui/zoomable-pdf";
@@ -10,20 +11,28 @@ interface OrgStructureChartProps {
 
 export const OrgStructureChart = ({ zoom }: OrgStructureChartProps) => {
     const { data, isPending, isError } = useOrgStructureImage();
+    const addNotification = useNotificationStore((state) => state.add);
 
     if (isPending) return <Skeleton className="w-full h-full" />;
 
-    if (isError || !data)
+    if (isError || !data) {
+      addNotification({
+        iconType: "error",
+        type: "error",
+        title: "Не удалось загрузить схему",
+        message: "Попробуйте обновить страницу позже",
+      });
+
       return (
         <div className="flex h-full items-center justify-center text-muted-foreground">
           Не удалось загрузить схему
         </div>
       );
+    }
 
-    if (data) 
-      return (
-        isPdf(data.image_url)
-          ? <ZoomablePDF src={data.image_url} zoom={zoom} className="h-full" />
-          : <ZoomableImage src={data.image_url} zoom={zoom} alt="Оргструктура"/>
-      )
+    return (
+      isPdf(data.image_url)
+        ? <ZoomablePDF src={data.image_url} zoom={zoom} className="h-full" />
+        : <ZoomableImage src={data.image_url} zoom={zoom} alt="Оргструктура"/>
+    )
 }
